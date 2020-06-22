f28ef406248eb97366599ed9150562d9994171e6
Jonathan Adriano <alexisjonathan378@gmail.com>
2020-06-22 17:31:07 +0000

    Agregado mes en el calculo

--- edad.py
+++ edad.py
@@ -1,8 +1,12 @@
 #codigo ejemplo git
 #Autor:jonathan Adriano
 anio_actual= 2020
+mes_actual=7
 nombre =input("cual es tu nombre?:")
 anio_nacimiento= int(input("en que año naciste?:"))
-
+mes_nacimiento= int(input("en que mes naciste"))
 edad= anio_actual - anio_nacimiento
-print("hola", nombre, "tu edad en este año sera", edad);
+if mes_nacimiento > mes_actual:
+    edad -=1
+print("hola", nombre, "tu edad es", edad);
+
