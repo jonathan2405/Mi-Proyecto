d22253a4fdd934437158fd89fd4ca914f45644bc
Jonathan Adriano <alexisjonathan378@gmail.com>
2020-06-22 17:20:07 +0000

    comit inicial

---
+++ edad.py
@@ -0,0 +1,8 @@
+#codigo ejemplo git
+#Autor:jonathan Adriano
+anio_actual= 2020
+nombre =input("cual es tu nombre?:")
+anio_nacimiento= int(input("en que año naciste?:"))
+
+edad= anio_actual - anio_nacimiento
+print("hola", nombre, "tu edad en este año sera", edad);
